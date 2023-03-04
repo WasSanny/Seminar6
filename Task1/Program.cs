e5c8b94da9a60a3fdb6da10d7e14301db9846074
Dmitry Vasiliuk <usy222@mail.ru>
2023-03-04 22:27:04 +0300

    Программа, которая переворачивает массив

---
+++ Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Программа, которая перевернет одномерный массив
+//(1-й элемент на последний, последний на первый)
+
+int[] array = new int[5];
+RandomArr(array);
+Console.WriteLine(string.Join(" ", array));
+
+Switch(array);
+Console.WriteLine(string.Join(" ", array));
+
+void RandomArr(int [] array)
+{
+  Random random = new Random(); // объект генератора чисел
+
+  for(int i = 0; i < array.Length; i++)
+  {
+    array[i] = random.Next(); // массив заполненслучайными значениями
+  }
+
+}
+
+void Switch(int [] array) // переворачиваем массив
+{
+  int a = 0; // объявляем доп переменную для переворота масиива
+
+  for (int i = 0; i < array.Length/2; i++)
+  {
+    a = array[i];
+    array[i] = array[array.Length -1 - i];
+    array[array.Length -1 - i] = a;
+  }
+
+}
