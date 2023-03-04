84fa3b43cc06185dcc29e37da4cb87a9d20e4b83
Dmitry Vasiliuk <usy222@mail.ru>
2023-03-04 23:57:52 +0300

    Преобразовываем двоичное в десятичное число

---
+++ Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Программа, которая будет преобразовывать десятичное число в двоичное!
+
+int EnterNumber(string argument)
+{
+  int num;
+
+  Console.Write($"Введите число {argument}: ");
+
+  while(!int.TryParse(Console.ReadLine(), out num))
+  {
+    Console.WriteLine("Вы ввели не целое число");
+    Console.Write($"Повторите попытку! {argument}: ");
+  }
+  return num;
+
+}
+
+string GetTransform (int dec)
+{
+  string binn = "";
+
+  while(dec > 0)
+  {
+    binn = dec % 2 + binn;
+    dec = dec / 2; // dec /= 2
+  }
+  Console.WriteLine(binn);
+  return binn;
+}
+
+GetTransform(EnterNumber(""));
